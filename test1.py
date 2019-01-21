2ca057e486248cc8d56feb809d03ce02ed842410
Станислав Гребенюков <stanislav@grebenyukov.ru>
2019-01-21 16:28:25 +0400

    test4 start

--- test1.py
+++ test1.py
@@ -1,8 +1,51 @@
-userChoice = int(input("Input number: \n"))
+from tabulate import tabulate
 
-if userChoice < 0:
-    print('Shit')
-elif userChoice == 0:
-    print('You choice is 0')
-else:
+def test1():
+    myList = ['cat', 'dog', 'duck']
+
+    for elem in myList[:]:
+        if len(elem) > 3:
+            print(elem)
+            myList.append(elem)
+
+    print(myList)
+
+
+def test2():
+    print(tabulate([['Alice', 24], ['Bob', 19]], headers=['Name', 'Age']))
+
+def test3():
+    colomns = []
+    ans = ''
+    menu = "Введите через запятую имя и возраст. Если хотите закончить, введите 'end'\n"
+    while ans != 'end':
+        ans: str = input(menu)
+        if ans == 'end':
+            print('Exit from the app')
+            break
+        else:
+            ans_words = ans.replace(' ', '').split(',')
+
+            if len(ans_words) == 2:
+                try:
+                    ans_words[1] = int(ans_words[1])
+                except ValueError:
+                    print('Second arg do not int')
+
+                except IndexError:
+                    print('Just one arg')
+
+                if isinstance(ans_words[1], int):
+                    colomns.append(ans_words)
+
+            else:
+                    print('Don\'t recognized format')
+
+    print(tabulate(colomns, headers=['Name', 'Age']))
+
+
+def test4():
     pass
+
+
+test4()
